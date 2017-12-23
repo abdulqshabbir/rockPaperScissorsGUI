ac8b7a5e94dd007edccc92db86905e7e702d0a21
Abdul Shabbir <abdulqshabbir@gmail.com>
2017-12-23 19:43:32 +0300

    player and computer images added when round starts.  Player and computer images also removed when the 'play again' button is clicked.

--- script.js
+++ script.js
@@ -4,34 +4,55 @@ const scissorsButton = document.getElementById('scissors');
 
 let gamePaused = false;
 
-rockButton.addEventListener('click', function(e) {
-  if(gamePaused === false) {
-    const playerChoice = e.target.innerHTML;
-    playOneRound(playerChoice);
-  }
-});
-
-paperButton.addEventListener('click', function(e) {
-  if (gamePaused === false) {
-    const playerChoice = e.target.innerHTML;
-    playOneRound(playerChoice);
-  }
-});
-
-scissorsButton.addEventListener('click', function(e) {
-  if(gamePaused === false) {
-    const playerChoice = e.target.innerHTML;
-    playOneRound(playerChoice);
-  }
-});
-
 function playOneRound(playerChoice) {
   let computerChoice = computerPlay();
   let result = checkForWins(playerChoice, computerChoice);
+
   displayChoices(playerChoice, computerChoice);
   displayWinner(result);
+  displayPlayerImage(playerChoice);
+  displayComputerImage(computerChoice);
   gamePaused = true;
   playAgain();
+}
+
+function displayComputerImage(computerChoice) {
+    const imageDiv = document.getElementById('images');
+    const computerImg = document.createElement('img');
+    computerImg.setAttribute('id', 'computerImage');
+
+    if (computerChoice === 'Rock') {
+      computerImg.setAttribute('src', 'rock.jpg');
+      imageDiv.appendChild(computerImg);
+    }
+    else if (computerChoice === 'Scissors') {
+      computerImg.setAttribute('src', 'scissors.gif');
+      imageDiv.appendChild(computerImg);
+    }
+    else if(computerChoice === 'Paper') {
+      computerImg.setAttribute('src', 'paper.jpg');
+      imageDiv.appendChild(computerImg);
+    }
+}
+
+function displayPlayerImage(playerChoice) {
+  const imageDiv = document.getElementById('images');
+  const playerImg = document.createElement('img');
+
+  playerImg.setAttribute('id', 'playerImage');
+
+  if (playerChoice === 'Rock') {
+    playerImg.setAttribute('src', 'rock.jpg');
+    imageDiv.appendChild(playerImg);
+  }
+  else if (playerChoice === 'Scissors') {
+    playerImg.setAttribute('src', 'scissors.gif');
+    imageDiv.appendChild(playerImg);
+  }
+  else if(playerChoice === 'Paper') {
+    playerImg.setAttribute('src', 'paper.jpg');
+    imageDiv.appendChild(playerImg);
+  }
 }
 
 function playAgain() {
@@ -43,21 +64,32 @@ function playAgain() {
   playAgainButton.addEventListener('click', function(e) {
     removeElementsForANewGame(playAgainButton);
     gamePaused = false;
+    addTitleBack();
   });
+}
+
+function addTitleBack() {
+  const title = document.getElementById('chooseYourWeapon');
+  title.innerHTML = 'Choose Your Weapon!';
 }
 
 function removeElementsForANewGame(playAgainButton) {
   const playerChoice = document.getElementById('playerChoice');
   const computerChoice = document.getElementById('computerChoice');
+  const computerImage = document.getElementById('computerImage');
+  const playerImage = document.getElementById('playerImage');
 
   playAgainButton.remove();
   playerChoice.remove();
-  computerChoice.remove(); 
+  computerChoice.remove();
+  computerImage.remove();
+  playerImage.remove(); 
 }
 
 function displayWinner(winner) {
   const whoIsWinner = document.getElementById('chooseYourWeapon');
   whoIsWinner.innerHTML = winner;
+  whoIsWinner.classList.add('title');
 }
 
 function displayChoices(playerChoice, computerChoice) {
@@ -82,26 +114,26 @@ function computerPlay() {
 
 function checkForWins(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return 'tie';
+    return "It's a tie!";
   }
 
   else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-    return 'player win';
+    return 'Player wins!';
   }
   else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-    return 'player win';
+    return 'Player wins!';
   }
   else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-    return 'player win';
+    return 'Player wins!';
   }
   else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
-    return 'computer win';
+    return 'Computer wins!';
   }
   else if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
-    return 'computer win';
+    return 'Computer wins!';
   }
   else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
-    return 'computer win';
+    return 'Computer wins!';
   }
   else if (playerSelection !== 'Rock' && playerSelection !== 'Scissors' && playerSelection !== 'Paper') {
     alert("You did not enter a valid input!");
@@ -109,6 +141,26 @@ function checkForWins(playerSelection, computerSelection) {
   }
 }
 
+rockButton.addEventListener('click', function(e) {
+  if(gamePaused === false) {
+    const playerChoice = e.target.innerHTML;
+    playOneRound(playerChoice);
+  }
+});
+
+paperButton.addEventListener('click', function(e) {
+  if (gamePaused === false) {
+    const playerChoice = e.target.innerHTML;
+    playOneRound(playerChoice);
+  }
+});
+
+scissorsButton.addEventListener('click', function(e) {
+  if(gamePaused === false) {
+    const playerChoice = e.target.innerHTML;
+    playOneRound(playerChoice);
+  }
+});
 
 
 
